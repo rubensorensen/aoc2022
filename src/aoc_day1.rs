41d6a20aaaebc3b8a829fa403d2b81f230a56a2f
Ruben Sørensen <ruben.sorensen98@gmail.com>
2022-12-01 19:22:32 +0100

    Generalized reused code

--- src/aoc_day1.rs
+++ src/aoc_day1.rs
@@ -2,7 +2,7 @@ use crate::aoc_utils;
 
 static FILEPATH: &str = "../data/day1_input.txt";
 
-pub fn part_1() {
+fn get_total_per_elf() -> Vec<u32> {
     let file_content = aoc_utils::slurp_file(FILEPATH);
 
     let mut cals: Vec<u32> = Vec::new();
@@ -16,24 +16,18 @@ pub fn part_1() {
         cal += line.parse::<u32>().unwrap();
     }
     cals.push(cal);
-    println!("Part 1: {}", cals.iter().max().unwrap());
+
+    return cals;
+}
+
+pub fn part_1() {
+    let cals = get_total_per_elf();
+    let max = *(cals.iter().max().unwrap());
+    println!("Part 1: {}", max);
 }
 
 pub fn part_2() {
-    let file_content = aoc_utils::slurp_file(FILEPATH);
-
-    let mut cals: Vec<u32> = Vec::new();
-    let mut cal: u32 = 0;
-    for line in file_content.lines() {
-        if line.is_empty() {
-            cals.push(cal);
-            cal = 0;
-            continue;
-        }
-        cal += line.parse::<u32>().unwrap();
-    }
-    cals.push(cal);
-
+    let mut cals = get_total_per_elf();
     cals.sort();
     let sum: u32 = cals.iter().rev().take(3).sum();
     println!("Part 2: {}", sum);
